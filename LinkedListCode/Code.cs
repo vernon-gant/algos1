43aa0f908302f53f8ba499331ab471715e8c3788
aleks.zakharov <aleks.zakharov@protonmail.com>
2023-08-18 19:05:31 +0200

    Final renaming

--- LinkedListCode/Code.cs
+++ LinkedListCode/Code.cs
@@ -1,20 +1,31 @@
 using System.Collections.Generic;
 
-namespace AlgorithmsDataStructures {
-    public class Node {
+namespace AlgorithmsDataStructures
+{
+    public class Node
+    {
+
         public int value;
+
         public Node next;
 
-        public Node(int _value) {
+        public Node(int _value)
+        {
             value = _value;
         }
+
     }
 
-    public class LinkedList {
-        public Node head;
-        public Node tail;
+    public class LinkedList
+    {
 
-        public LinkedList() {
+        public Node head;
+
+        public Node tail;
+        
+
+        public LinkedList()
+        {
             head = null;
             tail = null;
         }
@@ -24,50 +35,63 @@ namespace AlgorithmsDataStructures {
             this.head = head;
         }
 
-        public void AddInTail(Node _item) {
+        public void AddInTail(Node _item)
+        {
             if (head == null) head = _item;
             else tail.next = _item;
             tail = _item;
         }
 
-        public Node Find(int _value) {
+        public Node Find(int _value)
+        {
             Node node = head;
-            while (node != null) {
+
+            while (node != null)
+            {
                 if (node.value == _value) return node;
+
                 node = node.next;
             }
 
             return null;
         }
 
-        public List<Node> FindAll(int _value) {
+        public List<Node> FindAll(int _value)
+        {
             List<Node> nodes = new List<Node>();
+
             // здесь будет ваш код поиска всех узлов по заданному значению
             return nodes;
         }
 
-        public bool Remove(int _value) {
+        public bool Remove(int _value)
+        {
             // здесь будет ваш код удаления одного узла по заданному значению
             return true; // если узел был удалён
         }
 
-        public void RemoveAll(int _value) {
+        public void RemoveAll(int _value)
+        {
             // здесь будет ваш код удаления всех узлов по заданному значению
         }
 
-        public void Clear() {
+        public void Clear()
+        {
             // здесь будет ваш код очистки всего списка
         }
 
-        public int Count() {
+        public int Count()
+        {
             return 0; // здесь будет ваш код подсчёта количества элементов в списке
         }
 
-        public void InsertAfter(Node _nodeAfter, Node _nodeToInsert) {
+        public void InsertAfter(Node _nodeAfter, Node _nodeToInsert)
+        {
             // здесь будет ваш код вставки узла после заданного
 
             // если _nodeAfter = null , 
             // добавьте новый элемент первым в списке 
         }
+
     }
 }
