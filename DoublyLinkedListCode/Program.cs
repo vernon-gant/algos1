23713261e978b42ad130b61dde37b3b0af6dcf7d
aleks.zakharov <aleks.zakharov@protonmail.com>
2023-08-20 10:35:26 +0200

    Advanced list solution

--- DoublyLinkedListCode/Program.cs
+++ DoublyLinkedListCode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using AlgorithmsDataStructures;
 
 namespace DoublyLinkedListCode
 {
@@ -7,7 +8,19 @@ namespace DoublyLinkedListCode
 
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var list = new AdvancedLinkedList();
+            list.AddInTail(new Node(1));
+            list.AddInTail(new Node(2));
+            list.AddInTail(new Node(3));
+            list.AddInTail(new Node(4));
+            list.AddInTail(new Node(5));
+            Console.WriteLine(list.RemoveNode(6));
+            list.RemoveNode(5);
+            list.RemoveNode(4);
+            list.RemoveNode(3);
+            list.RemoveNode(2);
+            list.RemoveNode(1);
+            Console.WriteLine(list.RemoveNode(1));
         }
 
     }
