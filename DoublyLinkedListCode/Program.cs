109566bce579bb4a639ac7acc40cf6b3e7238da4
aleks.zakharov <aleks.zakharov@protonmail.com>
2023-08-20 09:32:41 +0200

    Doubly Linked Done!

---
+++ DoublyLinkedListCode/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DoublyLinkedListCode
+{
+    static class Program
+    {
+
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
