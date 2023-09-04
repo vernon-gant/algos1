62f50eebda6129f00a7c92431497525f876ffe03
aleks.zakharov <aleks.zakharov@protonmail.com>
2023-09-04 13:12:23 +0200

    Added hashtable solution

---
+++ HashTableCode/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AlgorithmsDataStructures;
+
+namespace HashTableCode
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var table = new HashTable(19, 3);
+            // Same
+            table.Put("Etergerb");
+            table.Put("aeqrew");
+            table.Put("aeqrew");
+            
+            table.Put("erhtrnbfnz");
+            table.Put("Test");
+            table.Put("Aleks");
+            table.Put("Rafek");
+        }
+
+    }
+}
