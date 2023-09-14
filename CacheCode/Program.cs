93eb4feb7f1b623ebe53564c6b9a0f704f95b862
aleks.zakharov <aleks.zakharov@protonmail.com>
2023-09-14 09:48:06 +0200

    Init cache solution

---
+++ CacheCode/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CacheCode
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var cache = new NativeCache<string>(8);
+            cache.Put("kok","ses");
+            cache.Put("kek","ses");
+            cache.Put("kak","ses");
+            cache.Put("kbk","ses");
+            cache.Put("ktk","ses");
+            cache.Put("sfregerdgrg","ses");
+            cache.Put("ergerg","ses");
+            cache.Put("ergergerg","ses");
+            cache.Put("cvbbf","ses");
+            cache.Put("lihkuihjkh","ses");
+            cache.Put("uyuijnnn","ses");
+            cache.Get("ktk");
+            cache.Get("ktk");
+            cache.Get("ktk");
+            cache.Get("ktk");
+            cache.Get("ktk");
+            cache.Get("kak");
+            cache.Get("kak");
+            cache.Get("kbk");
+            cache.Get("kbk");
+            cache.Get("kbk");
+            cache.Get("sfregerdgrg");
+            cache.Get("ergerg");
+            cache.Get("ergergerg");
+            cache.Get("cvbbf");
+            cache.Get("lihkuihjkh");
+            cache.Get("uyuijnnn");
+            cache.Put("kokozavr","test");
+        }
+
+    }
+}
--- DictionaryCode/Program.cs
+++ DictionaryCode/Program.cs
@@ -1,23 +1,9 @@
-﻿using System;
-using AlgorithmsDataStructures;
-
-namespace DictionaryCode
+﻿namespace DictionaryCode
 {
-    internal record Customer(string name, int age, double salary){}
-    
     class Program
     {
 
-        static void Main(string[] args)
-        {
-            var dictionary = new NativeDictionary<Customer>(19);
-            dictionary.Put("Bob", new Customer("Bob", 22, 2000.5));
-            dictionary.Put("Aleks", new Customer("Aleks", 25, 5500.5));
-            dictionary.Put("Martin", new Customer("Martin", 20, 500.5));
-            dictionary.Put("Aleks", new Customer("Aleks", 30, 600.5));
-            
-            Console.WriteLine(dictionary.IsKey("BOOOB"));
-        }
+        static void Main(string[] args) { }
 
     }
 }
