3bf1a6131f98c58be0c41728aec10a8d7508fea9
aleks.zakharov <aleks.zakharov@protonmail.com>
2023-10-05 09:18:38 +0200

    Tree solution

---
+++ TreeCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TreeCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
