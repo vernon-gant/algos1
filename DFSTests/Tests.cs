669f15445de6cdbc4fb3bed2a4971a7e30fbab68
aleks.zakharov <aleks.zakharov@protonmail.com>
2023-10-27 09:14:09 +0200

    DFS first part

---
+++ DFSTests/Tests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DFSTests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMethod1() { }
+    }
+}
